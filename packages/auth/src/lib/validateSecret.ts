9e0d77bb363e1e67337071100d1597fe75575400
Hassan T <tauqeer@t2.world>
2022-08-19 15:08:17 +0500

    Modified the validation to just use the value as a boolean, if there is a value it will proceed adn create session

--- packages/auth/src/lib/validateSecret.ts
+++ packages/auth/src/lib/validateSecret.ts
@@ -9,12 +9,13 @@ export async function validateSecret(
   secret: string,
   dbItemAPI: KeystoneDbAPI<any>[string]
 ): Promise<{ success: false } | { success: true; item: { id: any; [prop: string]: any } }> {
+  console.log('validateSecret - ', secret, secretField, secretFieldImpl, identityField, identity)
   const item = await dbItemAPI.findOne({ where: { [identityField]: identity } });
   if (!item || !item[secretField]) {
     // See "Identity Protection" in the README as to why this is a thing
     await secretFieldImpl.generateHash('simulated-password-to-counter-timing-attack');
     return { success: false };
-  } else if (await secretFieldImpl.compare(secret, item[secretField])) {
+  } else if (!!secret) {
     // Authenticated!
     return { success: true, item };
   } else {
